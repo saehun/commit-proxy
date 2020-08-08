5af54c9ce1a3255d735195552730f201a860ef6c
minidonut <nycom13@gmail.com>
2020-08-09 05:49:25 +0900

    add command to push. register, help

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
-/**
- * .git/hooks/post-commit
-#!/bin/sh
-
-# execute command with last commit log
-git log -1 | commit-proxy
-
-# Add other post-commit hooks
- */
 import * as fs from 'fs';
 import * as path from 'path';
+import * as execa from "execa";
 import simpleGit from 'simple-git';
+
+// initialize global variable
+const proxyRepo = process.env.PROXY_REPO;
+if (!proxyRepo) {
+  throw new Error('proxy local repository is undefiend');
+}
+
+const git = simpleGit({
+  baseDir: proxyRepo,
+  binary: 'git',
+});
+
+const postCommitScript = `#!/bin/sh
+
+# execute command with last commit log
+git log -1 | commit-proxy proxy
+
+# Add other post-commit hooks
+`;
 
 const getString = (): Promise<string> => {
   return new Promise(resolve => {
@@ -26,36 +37,34 @@ const getString = (): Promise<string> => {
   });
 };
 
+/**
+ *
+ */
 const gitRoot = (cur = process.cwd()): string => {
   if (cur === '/') throw new Error(`Can't find git root`);
 
   const dir = fs.readdirSync(cur);
   if (dir.includes('.git')) {
-    return cur.split('/').slice(-1)[0];
+    return cur;
   } else {
     return gitRoot(path.join(cur, '..'));
   }
 };
 
+/**
+ * Copy commit
+ */
 const copyCommit = (projectName: string, commit: string): void => {
-  const proxyRepo = process.env.PROXY_REPO;
-  if (!proxyRepo) {
-    throw new Error('proxy local repository is undefiend');
-  }
-
-  const git = simpleGit({
-    baseDir: proxyRepo,
-    binary: 'git',
-  });
-
-  const { commitMessage, file } = appendFile(proxyRepo, projectName, commit);
+  const { commitMessage, file } = appendFile(projectName, commit);
 
   git.add(file);
   git.commit(commitMessage);
 };
 
+/**
+ *
+ */
 const appendFile = (
-  proxyRepo: string,
   projectName: string,
   commit: string
 ): { commitMessage: string; file: string } => {
@@ -75,9 +84,45 @@ const appendFile = (
   };
 };
 
+const push = (): void => {
+  git.push('origin', 'master');
+};
+
+const register = (): void => {
+  const postCommitPath = path.join(gitRoot(), '.git', 'hooks', 'post-commit');
+  if (fs.existsSync(postCommitPath)) {
+    console.error('./.git/hooks/post-commit is alreay exist');
+    process.exit(1);
+  } else {
+    fs.writeFileSync(postCommitPath, postCommitScript);
+    execa('chmod', ['+x', postCommitPath]);
+    console.log(postCommitPath, 'registered');
+  }
+};
+
+const help = (): void => {
+  console.log(`
+commit-proxy <command>
+
+command:
+   - register: Register current git
+   - push: Push proxy repo to origin
+   - proxy: proxy commit in current repository
+`);
+};
+
 // main
 (async (): Promise<void> => {
-  const commit = await getString();
-  const projectName = gitRoot();
-  copyCommit(projectName, commit);
+  const cmd = process.argv[2].trim();
+  if (cmd === 'push') {
+    push();
+  } else if (cmd === 'register') {
+    register();
+  } else if (cmd === 'proxy') {
+    const commit = await getString();
+    const projectName = gitRoot().split('/').slice(-1)[0];
+    copyCommit(projectName, commit);
+  } else {
+    help();
+  }
 })();
